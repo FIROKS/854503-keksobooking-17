14b24c8ab478cddf8cb5ef3fb818a6546370497a
Максим <firoks1@gmail.com>
2019-07-31 02:03:17 +0300

    Добавлен метод upload

--- js/backend.js
+++ js/backend.js
@@ -2,6 +2,7 @@
 
 (function () {
   var URL_LOAD = 'https://js.dump.academy/keksobooking/data';
+  var URL_UPLOAD = 'https://js.dump.academy/keksobooking';
 
   var loadPins = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
@@ -20,8 +21,26 @@
     xhr.send();
   };
 
+  var uploadForm = function (data, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess();
+      } else {
+        onError();
+      }
+    });
+
+    xhr.open('POST', URL_UPLOAD);
+    xhr.send();
+  };
+
   window.backend = {
-    load: loadPins
+    load: loadPins,
+    upload: uploadForm
   };
 
 })();
